d8a70c7582c9b0d42a7cd6d3d26d8aeea7b16237
cindytran1 <tran.cin@northeastern.edu>
2025-02-08 17:39:13 -0500

    fixed format

--- src/CineEats/Home/index.tsx
+++ src/CineEats/Home/index.tsx
@@ -4,11 +4,11 @@ export default function Home() {
      return (
           <div className="home-page">
 
-               <Card style={{ width: "100vw",height: "450px", margin: 0, padding: 0, marginTop: "-200px"}}>
+               <Card style={{ width: "100vw",height: "450px", margin: 0, padding: 0, marginTop: "-100px", border: "none"}}>
                     <Card.Img
                          src="/images/studio_g.png"
                          height={365}
-                         style={{ width: "100vw", height:"100%", objectFit: "cover", display: "block" }}
+                         style={{ width: "100vw", height:"100%", display: "block", border: "none"}}
                     />
                    <p className="wd_content">“Food always comes to those who love to cook” <div className="wd_content_movie ">-Ratatouille</div>
                    <Card.Text
