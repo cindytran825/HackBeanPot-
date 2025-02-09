6dd62fca2c64803cb662fbd832f0d83fd0213964
victoriaw2408 <victoriaw2408@gmail.com>
2025-02-09 05:14:53 -0500

    fixed home

--- src/CineEats/Home/index.tsx
+++ src/CineEats/Home/index.tsx
@@ -4,18 +4,18 @@ import "./stylehome.css";
 
 export default function Home() {
      return (
-          
-          <div >
-               
-              
 
-               <Card style={{ width: "100vw", height: "450px", margin: 0, padding: 0, marginTop: "-100px", border: "none",}}>
+          <div >
+
+
+
+               <Card style={{ width: "100vw", height: "450px", margin: 0, padding: 0, marginTop: "-100px", border: "none", }}>
                     <Card.Img
                          src="/images/studio_g.png"
                          height={365}
-                         style={{ width: "100vw", height: "100%", display: "block", border: "none", filter: 'blur(1.5px)'}}
+                         style={{ width: "100vw", height: "100%", display: "block", border: "none", filter: 'blur(1.5px)' }}
                     />
-                    <p className="wd_content" style={{ textAlign: "center"}}>“Food always comes to those who love to cook.” <div className="wd_content_movie ">-Ratatouille</div>
+                    <p className="wd_content" style={{ textAlign: "center" }}>“Food always comes to those who love to cook.” <div className="wd_content_movie ">-Ratatouille</div>
                          <Card.Text
                               className="row justify-content-center align-items-center wd_content_bio">  Ever watched a movie or show and wished you could taste the delicious dishes on screen? Our menu brings those iconic animated meals to life, letting you cook and savor the foods you’ve craved from your favorite films, shows, and games.
                          </Card.Text>
@@ -31,21 +31,21 @@ export default function Home() {
                     <div className="card-alignment">
                          <div style={{ width: 415, height: 482, left: 50, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
                          <div style={{ width: 415, height: 478, left: 50, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
-                         <div style={{ width: 340, height: 106, left: 85, top: 400, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 25, fontFamily: 'Murecho', fontWeight: '600', wordWrap: 'break-word' }}>Spongebob’s Krabby Patty</div>
+                         <div style={{ width: 340, height: 106, left: 85, top: 400, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 25, fontWeight: '600', wordWrap: 'break-word' }}>Spongebob’s Krabby Patty</div>
                          <img style={{ width: 415, height: 360, left: 50, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/spongebob.png" />
                     </div>
 
                     <div className="card-alignment">
-                         <div style={{ width: 415, height: 482, left: 530, top: 1, position: 'absolute', background: '#D9D9D9',  borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 415, height: 482, left: 530, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
                          <div style={{ width: 415, height: 478, left: 530, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
-                         <div style={{ width: 340, height: 106, left: 555, top: 380, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 25, fontFamily: 'Murecho', fontWeight: '600', wordWrap: 'break-word' }}>Princess and The Frog’s Beignets</div>
+                         <div style={{ width: 340, height: 106, left: 570, top: 380, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 25, fontWeight: '600', wordWrap: 'break-word' }}>Princess and The Frog’s Beignets</div>
                          <img style={{ width: 415, height: 360, left: 530, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Tiana.png" />
                     </div>
 
                     <div className="card-alignment">
-                         <div style={{ width: 415, height: 482, right: 50, top: 1, position: 'absolute', background: '#D9D9D9',  borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 415, height: 482, right: 50, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
                          <div style={{ width: 415, height: 478, right: 50, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
-                         <div style={{ width: 340, height: 106, right: 85, top: 400, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 25, fontFamily: 'Murecho', fontWeight: '600', wordWrap: 'break-word' }}>Naruto's Ichiraku Ramen</div>
+                         <div style={{ width: 340, height: 106, right: 85, top: 400, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 25, fontWeight: '600', wordWrap: 'break-word' }}>Naruto's Ichiraku Ramen</div>
                          <img style={{ width: 415, height: 360, right: 50, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/naruto.png" />
                     </div>
 
@@ -53,31 +53,69 @@ export default function Home() {
                     <div className="card-alignment-second">
                          <div style={{ width: 280, height: 308, left: 60, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
                          <div style={{ width: 290, height: 306, left: 50, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
-                         <div style={{ width: 400, height: 25,  top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Murecho', fontWeight: '600', wordWrap: 'break-word' }}>Ratatouille</div>
+                         <div style={{ width: 400, height: 25, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>Ratatouille</div>
                          <img style={{ width: 290, height: 240, left: 50, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Rata.png" />
                     </div>
                     <div className="card-alignment-second">
-                         <div style={{ width: 290, height: 308, left: 420, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
-                         <div style={{ width: 290, height: 306, left: 420, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
-                         <div style={{ width: 400, height: 25, left: 365, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Murecho', fontWeight: '600', wordWrap: 'break-word' }}>Fortnite's Durr Burger</div>
-                         <img style={{ width: 290, height: 240, left: 420, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Durr.png" />
+                         <div style={{ width: 290, height: 308, left: 410, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 290, height: 306, left: 410, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
+                         <div style={{ width: 400, height: 25, left: 355, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>Fortnite's Durr Burger</div>
+                         <img style={{ width: 290, height: 240, left: 410, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Durr.png" />
                     </div>
                     <div className="card-alignment-second">
-                         <div style={{ width: 290, height: 308, left: 790, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
-                         <div style={{ width: 290, height: 306, left: 790, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
-                         <div style={{ width: 400, height: 25, left: 735, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Murecho', fontWeight: '600', wordWrap: 'break-word' }}>Coraline's Cake</div>
-                         <img style={{ width: 290, height: 240, left: 790, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Coraline.png" />
+                         <div style={{ width: 290, height: 308, left: 770, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 290, height: 306, left: 770, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
+                         <div style={{ width: 400, height: 25, left: 715, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>Coraline's Cake</div>
+                         <img style={{ width: 290, height: 240, left: 770, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Coraline.png" />
                     </div>
                     <div className="card-alignment-second">
-                         <div style={{ width: 290, height: 308, right: 60, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 290, height: 308, right: 50, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
                          <div style={{ width: 290, height: 306, right: 50, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
-                         <div style={{ width: 400, height: 25, left: 1075, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'Murecho', fontWeight: '600', wordWrap: 'break-word' }}>The Office Kevin's Chilli</div>
+                         <div style={{ width: 400, height: 25, left: 1075, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>The Office's Chilli</div>
                          <img style={{ width: 290, height: 240, right: 50, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Kevin.png" />
+                    </div><br />
+
+                    <div className="card-alignment-third">
+                         <div style={{ width: 280, height: 308, left: 60, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 290, height: 306, left: 50, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
+                         <div style={{ width: 400, height: 25, top: 245, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>Demon Slayer's Yamakake <br />Udon</div>
+                         <img style={{ width: 290, height: 240, left: 50, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/DemonSlayer.png" />
                     </div>
+                    <div className="card-alignment-third">
+                         <div style={{ width: 290, height: 308, left: 410, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 290, height: 306, left: 410, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
+                         <div style={{ width: 400, height: 25, left: 355, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>My Little Pony's Pancakes</div>
+                         <img style={{ width: 290, height: 240, left: 410, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Pancakes.png" />
+                    </div>
+                    <div className="card-alignment-third">
+                         <div style={{ width: 290, height: 308, left: 770, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 290, height: 306, left: 770, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
+                         <div style={{ width: 400, height: 25, left: 715, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>iCarly's Spaghetti Taco</div>
+                         <img style={{ width: 290, height: 240, left: 770, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Taco.png" />
+                    </div>
+                    <div className="card-alignment-third">
+                         <div style={{ width: 290, height: 308, right: 50, top: 1, position: 'absolute', background: '#D9D9D9', borderRadius: 10, filter: 'blur(5px)' }} />
+                         <div style={{ width: 290, height: 306, right: 50, top: 0, position: 'absolute', background: '#F6F1EB', borderRadius: 10 }} />
+                         <div style={{ width: 400, height: 25, left: 1075, top: 260, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 20, fontWeight: '600', wordWrap: 'break-word' }}>Demon Slayer's Onigiri</div>
+                         <img style={{ width: 290, height: 240, right: 50, top: 0.42, position: 'absolute', borderTopLeftRadius: 10, borderTopRightRadius: 10 }} src="./images/Onigiri.png" />
+                    </div><br /><br /><br />
+
+                    <div className="email">
+                         <div style={{ textAlign: 'center', color: 'black', fontSize: 16, fontFamily: 'BIZ UDPMincho', fontWeight: '700', wordWrap: 'break-word' }}>Enjoy new weekly recipes and recommendations</div>
+                         <div style={{ textAlign: 'center', color: 'black', fontSize: 60, fontFamily: 'BIZ UDPMincho', fontWeight: '700', wordWrap: 'break-word' }}>Join our mailing list</div>
+                         <div>
+                              <div style={{ width: '100%', height: '100%', background: 'white', borderRadius: 10 }} />
+                              <div style={{ textAlign: 'center', color: 'rgba(0, 0, 0, 0.70)', fontSize: 20, fontFamily: 'BIZ UDPMincho', fontWeight: '700', wordWrap: 'break-word' }}>Email Address</div>
+                              <div style={{ textAlign: 'center', color: 'black', fontSize: 20, fontFamily: 'BIZ UDPMincho', fontWeight: '700', wordWrap: 'break-word' }}>Send</div>
+                              <div style={{ width: '100%', height: '100%', background: '#EAC7A7', borderRadius: 10 }} />
+
+                         </div>
+                    </div>
+
 
                </Card >
 
-               
+
 
 
           </div >
